83e3cad96f183691df25dc3f3eb44095f30be620
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-02-13 14:10:41 +0100

    zmiana bloku reporter:

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'always' }]],
+  //reporter: [['html', { outputFolder: 'playwright-report', open: 'always' }]],
+  reporter: [
+  ['line'],
+  ['html', {
+    outputFolder: 'playwright-report',
+    open: process.env.CI ? 'never' : 'always'
+  }]
+],
   /* Ustawienia współdzielone między wszystkimi projektami. 
      Zobacz: https://playwright.dev/docs/api/class-testoptions. */
   use: {
